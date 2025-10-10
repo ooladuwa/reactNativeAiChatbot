bd26a61d88c2ce73851fe236fb02ccd1e1f6fff0
Onaje Oladuwa <ooladuwa@gmail.com>
2025-10-10 17:49:36 -0400

    add nativewind for css styling

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: [
+      // React Native preset with jsxImportSource for nativewind
+      ['module:@react-native/babel-preset', { jsxImportSource: 'nativewind' }],
+      // NativeWind's babel preset (needed)
+      'nativewind/babel',
+    ],
+    plugins: [
+      // other plugins you need...
+      // react-native-reanimated plugin MUST be last if you use Reanimated:
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
